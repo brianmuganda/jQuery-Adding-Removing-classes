eb17a2155e69508e48e5984824f3d6fbb5f70d9b
brianmuganda <brian.ashiali@student.moringaschool.com>
2021-09-22 16:20:07 +0300

    comment out jquerry styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
+ //adding background color using jquery- this is not advisable as this a css function.
+    //  $("button#green").click(function(){
+  //      $("body").css("background-color", "green");
+  //  });
     $("button#green").click(function(){
-        $("body").css("background-color", "green");
-    });
+        $("body").removeClass();
+        $("body").addClass("green-background");
+});
     $("button#yellow").click(function(){
         $("body").removeClass();
         $("body").addClass("yellow-background");
